4d9b0f1f3a6bbb9ee994aff489aeafade2abe989
James Tarleton <jamestarleton@gmail.com>
2014-10-20 08:16:45 -0400

    update

--- apps/front/modules/main/templates/racesSuccess.php
+++ apps/front/modules/main/templates/racesSuccess.php
@@ -24,16 +24,16 @@
         </tfoot>
  
         <tbody>
-            
+            <?php for($i = 0; $i< 200; $i++): ?>
             <tr>
                 <td>2014-04-01</td>
                 <td>Scotland Run</td>
                 <td>10K</td>
                 <td>199</td>
-                <td>40:48</td>
+                <td><?php echo rand(1,9); ?>0:48</td>
                 <td>New York, NY</td>
             </tr>
- 
+            <?php endfor; ?>
         </tbody>
     </table>
 </body></html>
