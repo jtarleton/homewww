33426ac2bad8779a2fd6115e509905bbc5901a80
James Tarleton <jamestarleton@gmail.com>
2014-11-09 09:55:38 -0500

    updates

--- apps/front/modules/main/templates/aboutSuccess.php
+++ apps/front/modules/main/templates/aboutSuccess.php
@@ -1,4 +1,6 @@
 	<p>Hello, and welcome to my personal home page.</p>
 					<p>I am a PHP developer based in New York City.  My professional background includes symfony, Wordpress, and Drupal.</p>
 
-					<p>You can sometimes find me on <a href="">stackoverflow and <a href="">github</a>. Contact me <a href="">here</a>.  Thank you for stopping by.</p>
+					<p>You can sometimes find me on <a href="http://stackoverflow.com/users/1172735/sassboss">stackoverflow</a> and <a href="https://github.com/jtarleton">github</a>. Contact me <a href="contact">here</a>.</p>  
+
+						<p>Thank you for stopping by.</p>
