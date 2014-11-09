9e5983a499b4e111b3bf08498f60cecc0ae34915
James Tarleton <jamestarleton@gmail.com>
2014-11-09 11:15:17 -0500

    updates

--- apps/front/modules/main/templates/contactSuccess.php
+++ apps/front/modules/main/templates/contactSuccess.php
@@ -2,6 +2,33 @@
 textarea#msg {
 	width:683px; height:100px;
 }
+
+.button1 {
+ 
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 24px;
+    color: #FFF;
+    padding: 5px 10px 5px 10px;
+    border: solid 1px #CCC;
+ 
+    background: #ba4742;
+    text-shadow: 0px 1px 0px #000;
+ 
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    -webkit-border-radius: 5px;
+ 
+    box-shadow: 0 1px 3px #111;
+    -moz-box-shadow: 3px 3px 1px #999;
+    -webkit-box-shadow: 3px 3px 1px #999;
+ 
+    cursor: pointer;
+ 
+}
+.button1:hover {
+    background: #a33f3a;
+}
+
 </style>
 <h1>Contact Me</h1>
 <form action="http://www.jamestarleton.com/contact" method="post">
@@ -9,7 +36,7 @@ textarea#msg {
     <?php echo $form; ?>
     <tr>
       <td colspan="2">
-        <input value="Send" type="submit" style="border:1px solid #000;">
+        <input value="Send" type="submit" class="button1">
       </td>
     </tr>
   </tbody></table>
