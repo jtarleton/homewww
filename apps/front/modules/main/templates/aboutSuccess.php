8a248c93603310238922f01b70ca29eec309d731
James Tarleton <jamestarleton@gmail.com>
2014-11-09 09:50:16 -0500

    about page

---
+++ apps/front/modules/main/templates/aboutSuccess.php
@@ -0,0 +1,4 @@
+	<p>Hello, and welcome to my personal home page.</p>
+					<p>I am a PHP developer based in New York City.  My professional background includes symfony, Wordpress, and Drupal.</p>
+
+					<p>You can sometimes find me on <a href="">stackoverflow and <a href="">github</a>. Contact me <a href="">here</a>.  Thank you for stopping by.</p>
