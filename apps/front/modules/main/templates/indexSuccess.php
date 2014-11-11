17ddbcb2b46365f9895d3eb8e7e6c44023d437af
James Tarleton <jamestarleton@gmail.com>
2014-11-11 06:51:21 -0500

    updates

--- apps/front/modules/main/templates/indexSuccess.php
+++ apps/front/modules/main/templates/indexSuccess.php
@@ -1,6 +1,4 @@
 
- <h2>The Inland Wood CSS template</h2>
-                <p>This is Inland Wood, the second in a new series of HTML/CSS templates from <a href="http://andreasviklund.com/">andreasviklund.com</a>. It is a multi-layout design, and the second andreasviklund.com template to feature an image slider. Inland Wood is made for template users who want a design that catches the attention of the website visitor and it is suitable for both personal websites and blogs, as well as for professional websites for companies and organizations. Like all templates by Andreas Viklund, it keeps the code simple, making the template easy to customize and build upon. To see the different layout options, click the main menu links above...</p>
+ <b>November 11, 2014</b>
+                <p>Cool content coming soon...</p>
 
-
-<?php foreach($data as $item) echo $item;
