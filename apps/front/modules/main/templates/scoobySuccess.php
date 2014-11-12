4dabfbc58ae975d91cb0b8c53eb17de452203def
James Tarleton <jamestarleton@gmail.com>
2014-11-12 06:45:55 -0500

    updates

--- apps/front/modules/main/templates/scoobySuccess.php
+++ apps/front/modules/main/templates/scoobySuccess.php
@@ -1,2 +1,5 @@
 <h1>Scooby</h1>
+
+<p>Scooby is a blog application written in PHP using a MongoDB back end.  The data model resembles Wordpress, using a single posts collection and the shared taxonomy concept for categories and tags.
+
 <img src="images/Scooby-Doo-006.jpg"></img>
