99636d05467d8a30a4281335b12f3f7236cc3e31
James Tarleton <jamestarleton@gmail.com>
2014-11-17 07:27:01 -0500

    updates

--- apps/front/modules/main/actions/actions.class.php
+++ apps/front/modules/main/actions/actions.class.php
@@ -21,6 +21,10 @@ class mainActions extends sfActions
   }
   public function execute2colsleft(sfWebRequest $request){
   }
+  public function executeTutorials(){
+
+    //$this->setLayout('layout-2col-left');
+  }
 public function executePhpbooks(){
 
     //$this->setLayout('layout-2col-left');
