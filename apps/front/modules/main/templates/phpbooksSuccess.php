c0a26f216122367d8d1f2280a660b1f5ed737734
James Tarleton <jamestarleton@gmail.com>
2014-12-06 16:15:41 -0500

    update

--- apps/front/modules/main/templates/phpbooksSuccess.php
+++ apps/front/modules/main/templates/phpbooksSuccess.php
@@ -6,12 +6,12 @@
 
 
 
-	<li><?php echo link_to('PHP and MySQL Web Development, by Welling/Thomson', 'http://www.amazon.com/PHP-MySQL-Web-Development-4th/dp/0672329166'); ?></li>
+	<li><?php echo link_to('PHP and MySQL Web Development', 'http://www.amazon.com/PHP-MySQL-Web-Development-4th/dp/0672329166'); ?>, by Luke Welling/Laura Thomson</li>
 
 
 
 
-<li><?php echo link_to('Programming PHP', 'http://www.amazon.com/Programming-PHP-Rasmus-Lerdorf/dp/0596006810'); ?></li>
+<li><?php echo link_to('Programming PHP', 'http://www.amazon.com/Programming-PHP-Rasmus-Lerdorf/dp/0596006810'); ?>, by Lerdorf</li>
 
 
 </ul>
