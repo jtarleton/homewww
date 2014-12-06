98be43d67f0a25f0c807316f128de5495e213b7c
James Tarleton <jamestarleton@gmail.com>
2014-12-06 16:11:33 -0500

    update

--- apps/front/modules/main/templates/phpbooksSuccess.php
+++ apps/front/modules/main/templates/phpbooksSuccess.php
@@ -1,3 +1,3 @@
 <h1>Recommended PHP Books</h1>
 
-<ul><li><?php echo link_to('http://www.amazon.com/PHP-MySQL-Web-Development-4th/dp/0672329166', 'PHP and MySQL Web Development'); ?></li></ul>
+<ul><li><?php echo link_to('PHP and MySQL Web Development', 'http://www.amazon.com/PHP-MySQL-Web-Development-4th/dp/0672329166'); ?></li></ul>
